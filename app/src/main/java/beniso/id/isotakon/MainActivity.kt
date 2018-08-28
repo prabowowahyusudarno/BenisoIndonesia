4cb0ac0799e19017a28d5f156992a1cca3ef8284
Ahmad Fauzi Wibowo <ahmadfauziw97@gmail.com>
2018-08-28 13:47:04 +0700

    Completed layout for home and profile, working on onboarding and isoLes

--- app/src/main/java/beniso/id/isotakon/MainActivity.kt
+++ app/src/main/java/beniso/id/isotakon/MainActivity.kt
@@ -1,34 +1,60 @@
 package beniso.id.isotakon
 
+import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v7.widget.Toolbar
+import beniso.id.isotakon.modules.account.AccountFragment
+import beniso.id.isotakon.modules.isojawab.IsoJawabFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), IsoJawabFragment.OnFragmentInteractionListener, AccountFragment.OnFragmentInteractionListener {
+
+
+    private lateinit var toolbar: Toolbar
+    private lateinit var bottomNavigation: BottomNavigationView
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+            R.id.navigation_isojawab -> {
+                val isoJawabFragment = IsoJawabFragment.newInstance()
+                openFragment(isoJawabFragment)
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
+            R.id.navigation_isoles -> {
+
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+            R.id.navigation_akun -> {
+                val accountFragment = AccountFragment.newInstance()
+                openFragment(accountFragment)
                 return@OnNavigationItemSelectedListener true
             }
         }
         false
     }
 
+    private fun openFragment(fragment: Fragment) {
+        val transction = supportFragmentManager.beginTransaction()
+        transction.replace(R.id.container, fragment)
+        transction.commit()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        toolbar = findViewById(R.id.toolbar)
+        bottomNavigation = findViewById(R.id.bottomNavigation)
+
+        setSupportActionBar(toolbar)
+        val isoJawabFragment = IsoJawabFragment.newInstance()
+        openFragment(isoJawabFragment)
+        bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    override fun onFragmentInteraction(uri: Uri) {
+
     }
 }
