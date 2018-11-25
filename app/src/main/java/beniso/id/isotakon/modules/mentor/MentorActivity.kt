e62022d8b65b3a6e88c01f8850ff7bec53a7cd16
Ahmad Fauzi Wibowo <ahmadfauziw97@gmail.com>
2018-11-25 08:43:35 +0700

    Changed questions layout and added toolbar

--- app/src/main/java/beniso/id/isotakon/modules/mentor/MentorActivity.kt
+++ app/src/main/java/beniso/id/isotakon/modules/mentor/MentorActivity.kt
@@ -1,18 +1,13 @@
 package beniso.id.isotakon.modules.mentor
 
-import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.view.View
 import android.widget.ListView
-import android.widget.Toast
 import beniso.id.isotakon.R
 import beniso.id.isotakon.models.IsoJawabQuestion
-import beniso.id.isotakon.modules.sign_up.SignUpActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
-import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_mentor.*
 
 class MentorActivity : AppCompatActivity() {
@@ -50,8 +45,8 @@ class MentorActivity : AppCompatActivity() {
     }
 
     fun setAdapter(){
-        item_list.layoutManager = LinearLayoutManager(this)
-        item_list.adapter = MentorAdapter(this, questionList){
+        item_list_mentor.layoutManager = LinearLayoutManager(this)
+        item_list_mentor.adapter = MentorAdapter(this, questionList.asReversed()){
         }
     }
 }
